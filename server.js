1440b6865363bc6f1e0e7f2d6d7750340162a03d
harshithva <harshith11032001@gmail.com>
2020-05-11 19:59:55 +0530

    RootQuery created

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const expressGraphQl = require('express-graphql');
+const schema = require('./schema.js');
+
+
 const app = express()
 const port = 3000
 
+app.use('/graphql', expressGraphQl({
+    schema: schema,
+    graphiql: true
+}));
 app.get('/', (req, res) => res.send('Hello World!'))
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
